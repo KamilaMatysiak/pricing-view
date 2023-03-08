ee1e5e08d647a9c6a1c5d86629c5e0e75441e06c
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-03-09 00:19:02 +0100

    move styles to local file. Fix javascript file

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,8 +1,8 @@
 /// Homepage JavaScript File
 /// Here we import all the JavaScript files we need for our homepage.
 
-import '../styles/home-page.scss'
+import '../styles/home-page.css'
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+//import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
 
-setupCounter(document.querySelector('#counter'))
+//setupCounter(document.querySelector('#counter'))
